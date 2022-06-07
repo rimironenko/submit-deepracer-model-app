0ecd4bfbe9db5c7a8180b496dc6b71930d13254c
Rostyslav_Myronenko <rimironenko@gmail.com>
2022-06-07 19:55:59 +0300

    Removed the API and added a check for currently running jobs

--- submit_model/app.py
+++ submit_model/app.py
@@ -5,6 +5,10 @@ def lambda_handler(event, context):
     model_arn = event['ModelArn']
     leaderboard_arn = event['LeaderboardArn']
     dr_client = deepracer_client()
-    # models = dr_client.list_models(ModelType="REINFORCEMENT_LEARNING", MaxResults=100)["Models"]
-    return dr_client.create_leaderboard_submission(ModelArn=model_arn, LeaderboardArn=leaderboard_arn)
+    latest_submission = dr_client.get_latest_user_submission(LeaderboardArn=leaderboard_arn)['LeaderboardSubmission']
+    if latest_submission['LeaderboardSubmissionStatusType'] == 'SUCCESS':
+        print("No running leaderboard submissions were found. Creating a new one.")
+        return dr_client.create_leaderboard_submission(ModelArn=model_arn, LeaderboardArn=leaderboard_arn)
+    print('There is a currently running leaderboard submission. Please see its status as the lambda output.')
+    return latest_submission['LeaderboardSubmission']
 
