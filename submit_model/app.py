c32e603c8f7198bc313581c5726457b525ef520c
Rostyslav_Myronenko <rimironenko@gmail.com>
2022-06-07 17:49:08 +0300

    Initial commit

--- submit_model/app.py
+++ submit_model/app.py
@@ -2,7 +2,7 @@ from deepracer.boto3_enhancer import deepracer_client
 
 
 def lambda_handler(event, context):
-    dr_client = client = deepracer_client()
+    dr_client = deepracer_client()
     models = dr_client.list_models(ModelType="REINFORCEMENT_LEARNING", MaxResults=100)["Models"]
     return models
 
