bf617b42f1345d62fd04acc60aa62ea863e6ce1f
Rostyslav_Myronenko <rimironenko@gmail.com>
2022-06-07 19:15:37 +0300

    Added Lambda policy and call for a model submission

--- submit_model/app.py
+++ submit_model/app.py
@@ -2,7 +2,9 @@ from deepracer.boto3_enhancer import deepracer_client
 
 
 def lambda_handler(event, context):
+    model_arn = event['ModelArn']
+    leaderboard_arn = event['LeaderboardArn']
     dr_client = deepracer_client()
-    models = dr_client.list_models(ModelType="REINFORCEMENT_LEARNING", MaxResults=100)["Models"]
-    return models
+    # models = dr_client.list_models(ModelType="REINFORCEMENT_LEARNING", MaxResults=100)["Models"]
+    return dr_client.create_leaderboard_submission(model_arn, leaderboard_arn)
 
