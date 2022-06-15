ecd78024037162d392ccbe8f503722e7184566a0
Rostyslav_Myronenko <rimironenko@gmail.com>
2022-06-15 12:27:34 +0300

    Updated tests

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,14 +1,13 @@
 import pytest
+import json
 
 from submit_model import app
 
 
 @pytest.fixture()
 def dr_submit_event():
-    return {
-        "ModelArn": "",
-        "LeaderboardArn": ""
-    }
+    test_event = open('./events/event.json')
+    return json.load(test_event)
 
 
 def test_lambda_handler(dr_submit_event, mocker):
