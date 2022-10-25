79871fe156c291834bbb5d65c6de95422da4039f
Rostyslav_Myronenko <rimironenko@gmail.com>
2022-10-25 12:44:19 +0200

    Added check for the error status

--- submit_model/app.py
+++ submit_model/app.py
@@ -6,9 +6,12 @@ def lambda_handler(event, context):
     leaderboard_arn = event['LeaderboardArn']
     dr_client = deepracer_client()
     latest_submission = dr_client.get_latest_user_submission(LeaderboardArn=leaderboard_arn)['LeaderboardSubmission']
-    if latest_submission['LeaderboardSubmissionStatusType'] == 'SUCCESS':
+    last_submission_status = latest_submission['LeaderboardSubmissionStatusType']
+    if last_submission_status == 'SUCCESS':
         print("No running leaderboard submissions were found. Creating a new one.")
         dr_client.create_leaderboard_submission(ModelArn=model_arn, LeaderboardArn=leaderboard_arn)
+    elif last_submission_status == 'FAILED' or last_submission_status == 'ERROR':
+        print("Latest submission was failed. Still creating a new one.")
+        dr_client.create_leaderboard_submission(ModelArn=model_arn, LeaderboardArn=leaderboard_arn)
     else:
-        print('There is a currently running leaderboard submission. Please see its status below.')
-        print(latest_submission)
+        print('There is a currently running leaderboard submission. Skipping.')
